433be6d169445908e8a6b0211398564fe7621c8a
citybizz-1 <saran89972@gmail.com>
2021-02-19 17:16:05 -0500

    initial commit

---
+++ src/component/Main/Main.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Main.css'
+
+export default function Main(){
+    return (
+        <div className="main" >
+            <p>
+            Well hello there! My name is Cassidy and I'm a Principal Developer Experience Engineer at Netlify. Before now, I worked at React Training, CodePen, Amazon, L4 Digital, Clarifai, and Venmo, and graduated from Iowa State University.
+I run a weekly newsletter and love teaching and helping people become better coders! In my spare time, I love playing music, building mechanical keyboards and LEGO sets, and hanging out with my sister Cami. We also like to make cool things with our laser cutter and 3D printer, and have a little Etsy shop if you want to check it out! I also have a Patreon if you'd like to chat with me regularly (our Discord group is so active I can hardly keep up, come join in the fun!), get resume reviews, schedule mock interviews with me, or get a few coupons and gifts from me on occasion. No pressure to join in, and I appreciate your support a ton no matter what!
+<br></br><br></br>
+In terms of where I am on the internet (and off), you might have seen me in Glamour Magazine or the Big Dream documentary a few years back, or more recently you might have seen my Scrabble keyboard project in a couple dozen different publications! I also made a successfully funded Kickstarter project called Go: On the Go! to get people interested in playing the game go. I've spoken at over a hundred events around the world, and have made online classes on Udemy, Skillshare, and moooore. I'll (likely) happily speak at your event or podcast, or do influencer work for your org. I have a speaker rider, if you'd like to talk to me about your event. Feel free to email me with details!
+Alright, since you've read this far, have a joke: Why are mountains so funny? Because they're hill areas.
+            
+            
+            
+            </p>
+
+            <p>
+            Well hello there! My name is Cassidy and I'm a Principal Developer Experience Engineer at Netlify. Before now, I worked at React Training, CodePen, Amazon, L4 Digital, Clarifai, and Venmo, and graduated from Iowa State University.
+I run a weekly newsletter and love teaching and helping people become better coders! In my spare time, I love playing music, building mechanical keyboards and LEGO sets, and hanging out with my sister Cami. We also like to make cool things with our laser cutter and 3D printer, and have a little Etsy shop if you want to check it out! I also have a Patreon if you'd like to chat with me regularly (our Discord group is so active I can hardly keep up, come join in the fun!), get resume reviews, schedule mock interviews with me, or get a few coupons and gifts from me on occasion. No pressure to join in, and I appreciate your support a ton no matter what!
+<br></br><br></br>
+In terms of where I am on the internet (and off), you might have seen me in Glamour Magazine or the Big Dream documentary a few years back, or more recently you might have seen my Scrabble keyboard project in a couple dozen different publications! I also made a successfully funded Kickstarter project called Go: On the Go! to get people interested in playing the game go. I've spoken at over a hundred events around the world, and have made online classes on Udemy, Skillshare, and moooore. I'll (likely) happily speak at your event or podcast, or do influencer work for your org. I have a speaker rider, if you'd like to talk to me about your event. Feel free to email me with details!
+Alright, since you've read this far, have a joke: Why are mountains so funny? Because they're hill areas.
+            
+            
+            
+            </p>
+        </div>
+    )
+}
